76b067ebfaf347db74944634a87a416ea7fc565d
Ben <smoov.b@gmail.com>
2025-05-20 10:46:40 -0500

    Create functions.php

---
+++ functions.php
@@ -0,0 +1,12 @@
+<?php
+// Load theme setup and features
+require get_template_directory() . '/inc/setup.php';
+
+// Enqueue scripts and styles
+require get_template_directory() . '/inc/scripts.php';
+
+// Custom Post Types (optional)
+require get_template_directory() . '/inc/custom-post-types.php';
+
+// Widgets
+require get_template_directory() . '/inc/widgets.php';
